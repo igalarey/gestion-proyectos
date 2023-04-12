001d6213743930ced21f918b04605c09708ce6e0
Imanol <igalarey@gmail.com>
2023-04-12 12:20:23 +0200

    Landing page acabada y hecha responsive en tablets.

--- script.js
+++ script.js
@@ -1,66 +1,88 @@
+const header = document.querySelector("header");
+const body = document.querySelector("body");
+
+const hamburguesa = document.querySelector("#hamburguesa");
+const op_nav = document.querySelector(".op_nav");
+
 const btn_login = document.getElementById("btn_login");
 const btn_registro = document.getElementById("btn_registro");
-const fondo_opaco = document.getElementById("fondo_opaco");
+const empezar = document.getElementById("empezar");
+
 const form_login = document.getElementById("form_login");
 const form_registro = document.getElementById("form_registro");
 const form_login_btn_regis = document.getElementById("form_login_btn_regis");
 const form_regis_btn_login = document.getElementById("form_regis_btn_login");
-const header = document.querySelector("header");
+const fondo_opaco = document.getElementById("fondo_opaco");
+
+const funciones = document.querySelector('a[href="#panel2"]');
+const panel2 = document.querySelector("#panel2");
+const sobre_nosotros = document.querySelector('a[href="#nosotros"]');
+const nosotros = document.querySelector("#nosotros");
 
 function cambiar_header() {
   header.classList.toggle("activo", window.scrollY > 0);
 }
 
-function mostrar_form_login() {
+function mostrar_form(form) {
   fondo_opaco.style.display = "block";
-  form_login.style.display = "block";
-}
-
-function mostrar_form_registro() {
-  fondo_opaco.style.display = "block";
-  form_registro.style.display = "block";
+  form_login.style.display = form === "login" ? "block" : "none";
+  form_registro.style.display = form === "registro" ? "block" : "none";
+  body.style.overflow = "hidden";
 }
 
 function ocultar_forms() {
   fondo_opaco.style.display = 'none';
   form_login.style.display = "none";
   form_registro.style.display = "none";
+  body.style.overflow = "auto";
 }
 
-function cambiar_a_registro() {
-  form_login.style.display = "none";
-  form_registro.style.display = "block";
-}
-
-function cambiar_a_login() {
-  form_login.style.display = "block";
-  form_registro.style.display = "none";
+function cambiar_a_formulario(formulario) {
+  form_login.style.display = formulario === "registro" ? "none" : "block";
+  form_registro.style.display = formulario === "registro" ? "block" : "none";
 }
 
 window.addEventListener("scroll", cambiar_header);
 
-btn_login.addEventListener("click", function(event) {
+btn_login.addEventListener("click", (event) => {
   event.preventDefault();
-  mostrar_form_login();
+  mostrar_form("login");
 });
 
-btn_registro.addEventListener("click", function(event) {
+btn_registro.addEventListener("click", (event) => {
   event.preventDefault();
-  mostrar_form_registro();
+  mostrar_form("registro");
 });
 
-window.addEventListener('click', function(event) {
+empezar.addEventListener("click", (event) => {
+  event.preventDefault();
+  mostrar_form("registro");
+});
+
+window.addEventListener('click', (event) => {
   if (event.target === fondo_opaco) {
     ocultar_forms();
   }
 });
 
-form_login_btn_regis.addEventListener("click", function(event) {
+form_login_btn_regis.addEventListener("click", (event) => {
   event.preventDefault();
-  cambiar_a_registro();
+  cambiar_a_formulario("registro");
 });
 
-form_regis_btn_login.addEventListener("click", function(event) {
+form_regis_btn_login.addEventListener("click", (event) => {
   event.preventDefault();
-  cambiar_a_login();
+  cambiar_a_formulario("login");
+});
+
+hamburguesa.addEventListener("click", () => {
+  op_nav.classList.toggle("abierto")  
+});
+
+funciones.addEventListener('click', () => {
+  panel2.scrollIntoView({ behavior: "smooth" });
+});
+
+sobre_nosotros.addEventListener("click", () => {
+  nosotros.scrollIntoView({ behavior: "smooth" });
 });
